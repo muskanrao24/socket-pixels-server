3252d3dac09fdcdeeccd4f13689857ef8fdc09af
muskanrao24 <muskanrao1202@gmail.com>
2023-03-02 18:55:03 +0530

    Mongodb to persist data

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import { handleChatRoom } from "./controllers/chat.js";
 import socketioServer from "fastify-socket.io";
 import cors from "@fastify/cors";
 import fastify from "fastify";
+import clientPromise from "./lib/db/mongodb.js";
+import { config } from "./config.js";
 
 const app = Fastify({
   logger: true,
@@ -19,6 +21,7 @@ app.register(cors, {
 
 app.get("/", (request, response) => {
   // app.io.emit("Hello World!");
+  console.log({ ip: request.socket.remoteAddress });
   console.log("OI");
 });
 
@@ -28,24 +31,70 @@ app.ready((err) => {
   if (err) throw err;
   const roomio = app.io.of("/room");
   roomio.on("connect", (socket) => {
-    var address = socket.handshake.address;
-    console.log("New connection from " + address);
+    let userData = null;
+    let conn, db, users;
+    var ipAddress = socket.handshake.address;
+    console.log("New connection from " + ipAddress);
     console.info("Socket Connected!", socket.id);
     socket.onAny((event, ...args) => {
-      console.log(`${event} fired with args ${args}...`);
+      console.log(`${event} fired with args ${JSON.stringify(args)}...`);
     });
 
     socket.on("message", (data) => {
       console.log({ receivedData: data });
     });
 
-    socket.on("pixel-put", (data) => {
-      console.log({ data });
-      roomio.emit("pixel-update", {
-        row: data.row,
-        color: data.color || "pink",
-        column: data.column,
-      });
+    socket.on("pixel-put", async (...args) => {
+      let data = args[0];
+      if (!conn) {
+        conn = await clientPromise;
+        db = conn.db("socket-chat");
+        users = db.collection("users");
+      }
+
+      // let data = await users.find({}).toArray();
+
+      // console.log({ users: data });
+      let ipUser = userData || (await users.findOne({ ipAddress: ipAddress }));
+
+      userData = ipUser;
+
+      if (ipUser && Date.now() - userData.lastInputTime < config.timeout) {
+        console.log(
+          "SCAMM - wait till ",
+          config.timeout - Date.now() + userData.lastInputTime
+        );
+      } else {
+        console.log({ data });
+        if (userData) {
+          userData = { ipAddress: ipAddress, lastInputTime: Date.now() };
+        } else {
+          // New User IP Address
+          userData = { ipAddress: ipAddress, lastInputTime: Date.now() };
+        }
+        roomio.emit("pixel-update", {
+          row: data.row,
+          color: data.color || "pink",
+          column: data.column,
+        });
+
+        socket.on("disconnect", async (reason) => {
+          // Persist data to the database if the user exists
+          console.log("DISCONNECT");
+          if (userData) {
+            console.log("PERSISTING");
+            conn = await clientPromise;
+            db = conn.db("socket-chat");
+            users = db.collection("users");
+
+            users.updateOne(
+              { ipAddress: userData.ipAddress },
+              { $set: userData },
+              { upsert: true }
+            );
+          }
+        });
+      }
     });
   });
 });
